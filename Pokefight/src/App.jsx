29f8376e39793220d49104bb6b5f0f3dda85c8fd
Pranita Pendharkar <pendharkarpranita@gmail.com>
2023-10-09 15:21:43 +0200

    Added components

--- Pokefight/src/App.jsx
+++ Pokefight/src/App.jsx
@@ -1,7 +1,39 @@
 import "./App.css";
 
+import { Routes, Route } from "react-router-dom";
+import Getallpokemons from "./Components/Getallpokemons";
+import Pokemondetail from "./Components/Pokemondetail";
+import Search from "./Components/Search";
+import { useContext } from "react";
+import { ThemeContext } from "./Context/ThemeContext";
+
+
+import './App.css';
+import PokemonImage from "./Components/PokemonImage";
+import PokemonPage from "./Components/Pokemonpage";
+import Fight from "./Components/Fight"
+
+
+
+
 function App() {
-  return <></>;
+  
+   const [{theme,isDark,},toggleTheme]=useContext(ThemeContext);  
+    return ( <>  <div  style={{backgroundColor:theme.backgroundColor,color:theme.color}}>
+        <div >It's a {isDark ? "Dark" :"Light"} theme</div><br/>
+      
+   </div> <button onClick={toggleTheme}>Toggle Theme</button>
+
+ <Routes>
+  
+ <Route path="/pokemon" element={<Getallpokemons/>} />
+  
+  {/* <Route path="/Pokemon/:name/:type" element={ <PokemonPage/>} /> */}
+ 
+  <Route path="/Pokemon/:name/:type" element={ <Fight/>} />
+          </Routes>
+          <Search/>
+  </>);
 }
 
 export default App;
